4fdae6f81a60ee955c3891292554c00391764d54
Chrisagbara1 <chrisagbara2023@gmail.com>
2025-05-21 17:46:42 +0100

    Convert Hours to Seconds (edited the formular)

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,4 @@
 hours = 2
 #60 seconds in 1 minuites and 60 minuites in on hours, thus 60 × 60
-seconds = 60 * 60
-sum = seconds * hours
-print(hours, "hour(s) is", sum, "seconds")
+seconds = hours * 3600
+print(hours, "hour(s) is", seconds, "seconds")
