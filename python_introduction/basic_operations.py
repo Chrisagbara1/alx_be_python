14c65e29fd7445839d2204f5087fce647ea27dbf
Chrisagbara1 <chrisagbara2023@gmail.com>
2025-05-21 14:52:10 +0100

    Solving basic aritimetics..

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print ("The Addition of", number1, "and", number2, "is", sum)
+sum = number1 - number2
+print ("The Subtraction of", number1, "and", number2, "is", sum)
+sum = number1 * number2
+print ("The Multiplication of", number1, "and", number2, "is", sum)
